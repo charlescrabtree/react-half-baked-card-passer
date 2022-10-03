4a28e2dbd64bd54f78aa1272ec597535a5d3fdf3
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-10-03 16:56:59 -0700

    added state to gamecontext.js

---
+++ src/components/context/GameContext.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useContext } from 'react';
+import { createContext, useState } from 'react';
+
+const GameContext = createContext();
+
+const GameProvider = ({ children }) => {
+  const [selectedCard, setSelectedCard] = useState();
+
+  return <GameContext.Provider value={{
+    selectedCard,
+    setSelectedCard
+  }}>
+    {children}
+  </GameContext.Provider>;
+};
+
+const useGame = () => {
+  const context = useContext(GameContext);
+  if (context === undefined) {
+    throw new Error('whoops, useGame must be contained within GameProvider');
+  }
+  return context;
+};
+
+export { GameProvider, useGame };
