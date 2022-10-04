bc71354685f6d9d46468cc49bf5f6fd6710dc4a7
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-10-03 17:08:29 -0700

    refactored selectedCard and setSelectedCard

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GameContext } from '../context/GameContext';
 
 const suitMap = {
   hearts: '❤️',
@@ -7,7 +8,8 @@ const suitMap = {
   diamonds: '♦️',
 };
 
-export default function Card({ card, selectedCard, setSelectedCard, setFrom, player }) {
+export default function Card({ card, setFrom, player }) {
+  const { selectedCard, setSelectedCard } = useContext(GameContext);
   function handleCardClick() {
     setFrom(player);
     setSelectedCard(card);
--- src/components/ExecutePassButton.js
+++ src/components/ExecutePassButton.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { useContext } from 'react';
+import { GameContext } from '../context/GameContext';
 import Card from './Card';
 
 export default function ExecutePassButton({
   passCard,
   setFrom,
   from,
-  to,
-  selectedCard,
-  setSelectedCard,
+  to
 }) {
+  const { selectedCard, setSelectedCard } = useContext(GameContext);
   return (
     <div className="execute-button" onClick={() => passCard(selectedCard)}>
       Pass <Card card={selectedCard} setSelectedCard={setSelectedCard} setFrom={setFrom} /> from{' '}
