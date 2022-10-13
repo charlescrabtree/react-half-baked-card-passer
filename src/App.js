ecd9b3afc43efdba4fc3883950151ca7b48a6311
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-10-13 11:11:25 -0700

    removed unecessary state from App.js and added passcard function to line 61 in app.js

--- src/App.js
+++ src/App.js
@@ -52,52 +52,13 @@ function App() {
     <div className="App">
       <section>
         {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
-        <Player
-          to={to}
-          player={1}
-          hand={playerOneHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
-        />
-        <Player
-          to={to}
-          player={2}
-          hand={playerTwoHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
-        />
-        <Player
-          to={to}
-          player={3}
-          hand={playerThreeHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
-        />
-        <CardList
-          cards={deck}
-          selectedCard={selectedCard}
-          setSelectedCard={setSelectedCard}
-          setFrom={setFrom}
-          player={'deck'}
-        />
+        <Player player={1} hand={playerOneHand} />
+        <Player player={2} hand={playerTwoHand} />
+        <Player player={3} hand={playerThreeHand} />
+        <CardList cards={deck} player={'deck'} />
       </section>
       <section>
-        {selectedCard && (
-          <ExecutePassButton
-            passCard={passCard}
-            setFrom={setFrom}
-            from={from}
-            to={to}
-            selectedCard={selectedCard}
-            setSelectedCard={setSelectedCard}
-          />
-        )}
+        {selectedCard && <ExecutePassButton passCard={passCard} />}
       </section>
     </div>
   );
