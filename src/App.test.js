49c3c689e76bf69f51ae7b2dadadf196fb9a5a3a
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-10-13 11:20:44 -0700

    fixed pathing in app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { GameProvider } from './components/context/GameContext';
+import { GameProvider } from './context/GameContext';
 
 test('renders different players', () => {
   render(
